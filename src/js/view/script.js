3bdaef962111b5f5b3c82571281add4fd37d87a9
Juliane <julianebueno1@gmail.com>
2023-07-07 15:14:15 -0300

    atualizando pastas

---
+++ src/js/view/script.js
@@ -0,0 +1,71 @@
+// ============================================================= HEADER INDEX
+const headerIndex = document.getElementById("header-index");
+headerIndex.innerHTML = `
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#"><img src="https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top"> Some Enterprise </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-link active" aria-current="page" href="./src/html/login.html">Login</a>
+                <a class="nav-link" href="./src/html/cadastro.html">Sing up</a>
+            </div>
+        </div>
+    </div>
+</nav>
+`
+
+// ============================================================= HEADER LOGIN
+const headerLogin = document.getElementById("header-login");
+headerLogin.innerHTML = `
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#"><img src="https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top"> Some Enterprise </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-link active" aria-current="page" href="./src/html/login.html">Login</a>
+                <a class="nav-link" href="./src/html/cadastro.html">Sing up</a>
+            </div>
+        </div>
+    </div>
+</nav>
+`
+
+// ============================================================= HEADER CADASTRO
+const headerCadastro = document.getElementById("header-cadastro");
+headerCadastro.innerHTML = `
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#"><img src="https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top"> Some Enterprise </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-link active" aria-current="page" href="./src/html/login.html">Login</a>
+                <a class="nav-link" href="./src/html/cadastro.html">Sing up</a>
+            </div>
+        </div>
+    </div>
+</nav>
+`
+
+// ============================================================= HEADER INICIAL
+const headerInicial = document.getElementById("header-inicial");
+headerInicial.innerHTML = `
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#"><img src="https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top"> Some Enterprise </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-link active" aria-current="page" href="./src/html/login.html">Login</a>
+                <a class="nav-link" href="./src/html/cadastro.html">Sing up</a>
+            </div>
+        </div>
+    </div>
+</nav>
+`
+
+
+
+
